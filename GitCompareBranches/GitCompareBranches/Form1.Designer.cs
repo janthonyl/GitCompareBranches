0cc119ac64d3b8e2ef3b70903e522f922207166d
Jerry Laudat <janthonyl@yahoo.com>
2023-02-25 13:12:51 -0800

    First version of the form.

--- GitCompareBranches/GitCompareBranches/Form1.Designer.cs
+++ GitCompareBranches/GitCompareBranches/Form1.Designer.cs
@@ -28,20 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cboBranches2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboBranches1 = new System.Windows.Forms.ComboBox();
+            this.btnRepo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRepo = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(839, 422);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.LightSalmon;
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(831, 394);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.btnRefresh);
+            this.panel1.Controls.Add(this.cboBranches2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cboBranches1);
+            this.panel1.Controls.Add(this.btnRepo);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtRepo);
+            this.panel1.Location = new System.Drawing.Point(5, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(820, 69);
+            this.panel1.TabIndex = 3;
+            // 
+            // cboBranches2
+            // 
+            this.cboBranches2.FormattingEnabled = true;
+            this.cboBranches2.Location = new System.Drawing.Point(443, 33);
+            this.cboBranches2.Name = "cboBranches2";
+            this.cboBranches2.Size = new System.Drawing.Size(216, 23);
+            this.cboBranches2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(376, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Branches2";
+            // 
+            // cboBranches1
+            // 
+            this.cboBranches1.FormattingEnabled = true;
+            this.cboBranches1.Location = new System.Drawing.Point(70, 33);
+            this.cboBranches1.Name = "cboBranches1";
+            this.cboBranches1.Size = new System.Drawing.Size(246, 23);
+            this.cboBranches1.TabIndex = 5;
+            // 
+            // btnRepo
+            // 
+            this.btnRepo.AutoSize = true;
+            this.btnRepo.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnRepo.Location = new System.Drawing.Point(3, 3);
+            this.btnRepo.Name = "btnRepo";
+            this.btnRepo.Size = new System.Drawing.Size(44, 25);
+            this.btnRepo.TabIndex = 0;
+            this.btnRepo.Text = "Repo";
+            this.btnRepo.UseVisualStyleBackColor = true;
+            this.btnRepo.Click += new System.EventHandler(this.btnRepo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Branches1";
+            // 
+            // txtRepo
+            // 
+            this.txtRepo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRepo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtRepo.Location = new System.Drawing.Point(59, 0);
+            this.txtRepo.Name = "txtRepo";
+            this.txtRepo.ReadOnly = true;
+            this.txtRepo.Size = new System.Drawing.Size(758, 29);
+            this.txtRepo.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(831, 394);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.AutoSize = true;
+            this.btnRefresh.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnRefresh.Location = new System.Drawing.Point(750, 33);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(56, 25);
+            this.btnRefresh.TabIndex = 8;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(844, 438);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TextBox txtRepo;
+        private Button btnRepo;
+        private TabPage tabPage2;
+        private Panel panel1;
+        private Label label1;
+        private ComboBox cboBranches2;
+        private Label label2;
+        private ComboBox cboBranches1;
+        private Button btnRefresh;
     }
 }
--- GitCompareBranches/GitCompareBranches/Form1.cs
+++ GitCompareBranches/GitCompareBranches/Form1.cs
@@ -21,21 +21,29 @@ namespace GitCompareBranches
 
         }
 
+
         private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+           // pathToRepo = @"C:\A\Darsy-CMO";
+        }
+
+        private List<Commit> GetAllCommitsForThisBranch(string branch, string pathToRepo)
         {
             List<Commit> commits = new List<Commit>();
             string prefix = "@@##@@";
-            string args = $"log  -5  --format='<prefix>%h %s'  ";
-            args = args.Replace("<prefix>", prefix).Replace('\'', '\"');
+            string args = $"log  -5 <branch>  --format='<prefix>%h %s'  ";
+            args = args.Replace("<branch>", branch).Replace("<prefix>", prefix).Replace('\'', '\"');
             Process P = new Process();
             P.StartInfo = new ProcessStartInfo("git", args);
             P.StartInfo.RedirectStandardOutput = true; //Necessary to capture the output. 
-            P.StartInfo.WorkingDirectory = @"C:\A\Darsy-CMO";
+            P.StartInfo.WorkingDirectory = pathToRepo;
             P.StartInfo.UseShellExecute = false;
             P.Start();
             P.WaitForExit();
             string[] lines = P.StandardOutput.ReadToEnd().Split(new string[] { prefix }, StringSplitOptions.RemoveEmptyEntries);
-            foreach(string line in lines)
+            foreach (string line in lines)
             {
                 if (line.Length < 10) continue;
                 Commit commit = new Commit();
@@ -43,10 +51,39 @@ namespace GitCompareBranches
                 commit.msg = line.Substring(9);
                 commits.Add(commit);
             }
-            
-         
-
-
+            return commits;
         }
+
+        private void btnRepo_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() != DialogResult.OK) return;
+            txtRepo.Text = fbd.SelectedPath;
+            List<string> branches = GetListOfBranchesInThisRepo(fbd.SelectedPath);
+            cboBranches1.Items.Clear();
+            cboBranches2.Items.Clear();
+            foreach (string branch in branches)
+            {
+                cboBranches1.Items.Add(branch);              
+                cboBranches2.Items.Add(branch);
+            }
+        }
+
+        private List<string> GetListOfBranchesInThisRepo(string pathToRepo)
+        {
+            List<Commit> commits = new List<Commit>();            
+            string args = $"branch ";
+            Process P = new Process();
+            P.StartInfo = new ProcessStartInfo("git", args);
+            P.StartInfo.RedirectStandardOutput = true; //Necessary to capture the output. 
+            P.StartInfo.WorkingDirectory = pathToRepo;
+            P.StartInfo.UseShellExecute = false;
+            P.Start();
+            P.WaitForExit();
+            string[] branches = P.StandardOutput.ReadToEnd().Split(new char[] { '\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+            return branches.OrderBy(b=>b).ToList();
+        }
+
+
     }
 }
